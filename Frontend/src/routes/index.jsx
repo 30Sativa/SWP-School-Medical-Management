64b8e7d099ff2f9be2f8b0cb5019b4a24e701ff7
theanh <anhpntse184648@fpt.edu.vn>
2025-06-02 15:25:48 +0700

    Add: ManagerDashboardPage

--- Frontend/src/routes/index.jsx
+++ Frontend/src/routes/index.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "../pages/homepage/Homepage";
 import Login from "../pages/auth/Login";
+import ManagerDashboard from "../pages/manager/ManagerDashboard";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/manager" element={<ManagerDashboard />} />
     </Routes>
   );
 };
