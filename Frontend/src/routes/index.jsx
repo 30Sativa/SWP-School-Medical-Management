1fb00dcaeb4c239bd8dcd45d367df4e091e36232
theanh <anhpntse184648@fpt.edu.vn>
2025-06-04 12:22:30 +0700

    Add: UsersList

--- Frontend/src/routes/index.jsx
+++ Frontend/src/routes/index.jsx
@@ -2,13 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "../pages/homepage/Homepage";
 import Login from "../pages/auth/Login";
 import ManagerDashboard from "../pages/manager/ManagerDashboard";
-
+import UsersList from "../pages/manager/UsersList";
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/manager" element={<ManagerDashboard />} />
+       <Route path="/users" element={<UsersList />} />
     </Routes>
   );
 };
