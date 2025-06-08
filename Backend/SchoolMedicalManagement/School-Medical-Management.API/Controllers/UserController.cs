b1e382e3a630ab5082605d030ceff22b6f277ed1
30Sativa <thanhrrgame@gmail.com>
2025-06-08 21:35:15 +0700

    Fix: UserController

--- Backend/SchoolMedicalManagement/School-Medical-Management.API/Controllers/UserController.cs
+++ Backend/SchoolMedicalManagement/School-Medical-Management.API/Controllers/UserController.cs
@@ -78,11 +78,8 @@ namespace School_Medical_Management.API.Controllers
 
 
 
-        [HttpPost("change-password{id}")]
+        [HttpPost("change-password-firstlogin/{id}")]
         public async Task<IActionResult> ChangePasswordAfterFirstLogin([FromRoute] int id, SchoolMedicalManagement.Models.Request.ChangePasswordUser userChangePasswordRequest)
-
-        [HttpPost("change-password")]
-        public async Task<IActionResult> ChangePasswordAfterFirstLogin(UserChangePasswordRequest userChangePasswordRequest)
         {
             var response = await _authService.ChangePasswordAfterFirstLogin(id,userChangePasswordRequest);
             return StatusCode(int.Parse(response.Status), response);
