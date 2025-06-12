e466253fec273978eb35e6c102da93446e56c720
theanh <anhpntse184648@fpt.edu.vn>
2025-06-12 14:48:53 +0700

    HotFix: ManagerPages

--- Frontend/src/routes/index.jsx
+++ Frontend/src/routes/index.jsx
@@ -8,7 +8,7 @@ import StudentList from "../pages/nurse/StudentList";
 import StudentDetail from "../pages/nurse/StudentDetail";
 import ParentDashboard from "../pages/parent/ParentDashboard";
 import HealthProfile from "../pages/parent/HealthProfile";
-
+import LogsPage from "../pages/manager/Logs";
 import SendMedicine from "../pages/parent/SendMedicine";
 
 import VaccinCampaign from "../pages/nurse/VaccinCampaign";
@@ -25,7 +25,7 @@ const AppRouter = () => {
       <Route path="/students" element={<StudentList />} />
       <Route path="/students/:id" element={<StudentDetail />} />
       <Route path="/healthprofile" element={<HealthProfile />} />
-
+      <Route path="/logs" element={<LogsPage />} />
       <Route path="/sendmedicine" element={<SendMedicine />} />
       {/* Add more routes as needed */}
 
